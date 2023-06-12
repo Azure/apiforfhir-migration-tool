161ce1a3f19edce6a3389829c6cbe7f73c75b101
Mikael Weaver <mikaelw@microsoft.com>
2023-06-11 20:43:49 -0700

    Added base repository setup

---
+++ test/FhirMigrationTool.Test/Test.cs
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FhirMigrationTool.Tests
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void Placeholder()
+        {
+            var test = "one";
+
+            Assert.AreEqual("one", test);
+        }
+    }
+}
