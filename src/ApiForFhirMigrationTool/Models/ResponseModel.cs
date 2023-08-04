153fedb5a193cdf77447a1de75107abdb772e594
Mikael Weaver <mikaelw@microsoft.com>
2023-08-04 16:45:47 -0700

    Restructured projects and namespaces

---
+++ src/ApiForFhirMigrationTool/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace ApiForFhirMigrationTool.Models
+{
+    public class ResponseModel
+    {
+        public ResponseStatus Status { get; set; }
+
+        public string Content { get; set; } = string.Empty;
+    }
+}
