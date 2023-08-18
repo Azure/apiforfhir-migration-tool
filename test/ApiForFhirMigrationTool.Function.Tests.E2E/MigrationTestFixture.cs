6dbb02ee1b509cc88cb79639a43404c7cc15a723
Mikael Weaver <mikael@mikael.dev>
2023-08-18 13:26:22 -0700

    Updated E2E test - checkpoint

---
+++ test/ApiForFhirMigrationTool.Function.Tests.E2E/MigrationTestFixture.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
+using Microsoft.Health.Fhir.Tests.E2E.Rest;
+using Microsoft.Health.Fhir.Web;
+
+namespace ApiForFhirMigrationTool.Function.Tests.E2E
+{
+    public class MigrationTestFixture : HttpIntegrationTestFixture<Startup>
+    {
+        public MigrationTestFixture(DataStore dataStore, Format format, TestFhirServerFactory testFhirServerFactory)
+            : base(dataStore, format, testFhirServerFactory)
+        {
+        }
+    }
+}
