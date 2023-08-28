397bbb91aa89d7d85d552125c9a96fe801b12540
Shashank Narang <v-snarang@microsoft.com>
2023-08-28 17:51:02 +0530

    commented E2E test

--- test/ApiForFhirMigrationTool.Function.Tests.E2E/MigrationTests.cs
+++ test/ApiForFhirMigrationTool.Function.Tests.E2E/MigrationTests.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
+/*using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
 using Microsoft.Health.Fhir.Tests.E2E.Common;
 using Microsoft.Health.Fhir.Tests.E2E.Rest;
 using Xunit;
@@ -20,10 +20,10 @@ namespace ApiForFhirMigrationTool.Function.Tests.E2E
             _client = fixture.TestFhirClient;
         }
 
-        [Fact]
+        // [Fact]
         public async Task GivenATestServer_WhenMigrationOperation()
         {
-            /*
+
             var migrationResponse = await _client.GetAsync("/api/start-migration");
             migrationResponse.EnsureSuccessStatusCode();
             var migrationContent = await migrationResponse.Content.ReadAsStringAsync();
@@ -31,7 +31,7 @@ namespace ApiForFhirMigrationTool.Function.Tests.E2E
             var checkResponse = await _client.GetAsync("/api/run-check-operation");
             checkResponse.EnsureSuccessStatusCode();
             var checkContent = await checkResponse.Content.ReadAsStringAsync();
-            */
+
 
             var test = await _client.SearchAsync(Hl7.Fhir.Model.ResourceType.Patient);
 
@@ -39,3 +39,4 @@ namespace ApiForFhirMigrationTool.Function.Tests.E2E
         }
     }
 }
+*/
