6e94e429d23aa09024fb1335815b366aff55e88a
snarang-microsoft <109344265+snarang-microsoft@users.noreply.github.com>
2023-09-11 20:33:52 +0530

    Personal/snarang/v1.2 (#5)
    * Export Chunking changes
    
    * Updated the time to days
    
    * Test cases updated
    
    * Seperate suborchestrator for status check
    
    * Limiting the till to current date
    
    * Added arm template files
    
    * UI Form
    
    * Arm Template changes
    
    * Initial changes for search parameters migration
    
    * Reverted temporary changes
    
    * Added logs
    
    * Modified comment
    
    * Updated Unit test cases for V1.2
    
    * E2E Test case addition
    
    * Uncommented the time trigger for migration
    
    * Add search parameter activity in migration Orchestrator
    
    * changes for ARM template
    
    * Added test cases for search parameter migration changes
    
    * Moved the E2E test to new http function
    
    * Updated Unit test for mocking azure table
    
    * Added exepection handling and updating table for failure in export and import
    
    * Deleted the old E2E test cases
    
    * Restructured projects and namespaces
    
    * Fixed structure and tests
    
    * Fixed E2E test configuration
    
    * Added XUnit test cases for search parameter migration changes
    
    * template changes
    
    * Readme for V1.2
    
    * Update README.md
    
    * Template changes
    
    * UI Form validation fix
    
    * Bicep and ARM template changes
    
    * Changes for UI uiForm
    
    * Updated E2E test - checkpoint
    
    * update arm template for code deployment
    
    * code change for storing since after import completion
    
    * updating nuget.config for build error
    
    * changes to nuget config
    
    * reverting changes on nuget file
    
    * changing package source key for nuget
    
    * Removed the nuget.config file
    
    * Retry storage account connection, added start and end time in table
    
    * Updated the structure and infra code
    
    * Code zip
    
    * added nuget.config
    
    * change in nuget file
    
    * updated the Nuget
    
    * commented E2E test
    
    * Removed the package reference for E2E
    
    * updated build.props file
    
    * Commented roslynanalyzers
    
    * Updated the solution file name in checkcode
    
    * removed the build.prop
    
    * Infra changes
    
    * added entry check for chunk table
    
    * dashboard changes (#12)
    
    * telemetry code changes
    
    * bicep and arm template for dashboard
    
    * Changes to repair TelemetryClient dependency in unit tests
    
    * infra changes
    
    * update bicep
    
    ---------
    
    Co-authored-by: v-nmadwani <nitesh.madwani@xoriant.onmicrosoft.com>
    Co-authored-by: Ganesh Kuber <ganesh.kuber@xoriantcorp.onmicrosoft.com>
    Co-authored-by: Mikael Weaver <mikaelw@microsoft.com>
    Co-authored-by: Mikael Weaver <mikael@mikael.dev>
    Co-authored-by: v-nmadwani <116351573+v-nmadwani@users.noreply.github.com>

---
+++ src/ApiForFhirMigrationTool.Function/Configuration/MigrationOptions.cs
@@ -0,0 +1,107 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Azure.Core;
+using Azure.Identity;
+using Newtonsoft.Json;
+
+namespace ApiForFhirMigrationTool.Function.Configuration
+{
+    public class MigrationOptions
+    {
+        [JsonProperty("sourceFhirUri")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Uri SourceUri { get; set; }
+
+        [JsonProperty("destinationFhirUri")]
+        public Uri DestinationUri { get; set; }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        [JsonProperty("stagingStorageAccountName")]
+        public string StagingStorageAccountName { get; set; } = string.Empty;
+
+        [JsonProperty("stagingStorageUri")]
+        public string StagingStorageUri { get; set; } = string.Empty;
+
+        [JsonProperty("stagingContainerName")]
+        public string StagingContainerName { get; set; } = string.Empty;
+
+        [JsonProperty("scheduleInterval")]
+        public double ScheduleInterval { get; set; }
+
+        [JsonProperty("startDate")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("endDate")]
+        public string EndDate { get; set; } = string.Empty;
+
+        [JsonProperty("AppInsightConnectionString")]
+        public string AppInsightConnectionstring { get; set; } = string.Empty;
+
+        [JsonProperty("importMode")]
+        public string ImportMode { get; set; } = "IncrementalLoad";
+
+        public List<string>? SurfaceCheckResources { get; set; }
+
+        public List<string>? QuerySurface { get; set; }
+
+        public List<string>? QueryDeep { get; set; }
+
+        [JsonProperty("DeepCheckCount")]
+        public int DeepCheckCount { get; set; }
+
+        [JsonProperty("UserAgent")]
+        public string UserAgent { get; set; } = "FhirMigrationTool";
+
+        [JsonProperty("SourceHttpClient")]
+        public string SourceHttpClient { get; set; } = "SourceFhirEndpoint";
+
+        [JsonProperty("DestinationHttpClient")]
+        public string DestinationHttpClient { get; set; } = "DestinationFhirEndpoint";
+
+        [JsonProperty("TokenCredential")]
+        public TokenCredential TokenCredential { get; set; } = new DefaultAzureCredential();
+
+        [JsonProperty("retryCount")]
+        public int RetryCount { get; set; }
+
+        [JsonProperty("waitForRetry")]
+        public double WaitForRetry { get; set; }
+
+        [JsonProperty("debug")]
+        public bool Debug { get; set; }
+
+        [JsonProperty("exportTableName")]
+        public string ExportTableName { get; set; } = string.Empty;
+
+        [JsonProperty("chunkTableName")]
+        public string ChunkTableName { get; set; } = string.Empty;
+
+        [JsonProperty("ExportChunkTime")]
+        public int ExportChunkTime { get; set; } = 30;
+
+        [JsonProperty("partitionKey")]
+        public string PartitionKey { get; set; } = "mypartitionkey";
+
+        [JsonProperty("rowKey")]
+        public string RowKey { get; set; } = "myrowkey";
+
+        public bool ValidateConfig()
+        {
+            if (SourceUri != null
+                && DestinationUri != null
+                && !string.IsNullOrEmpty(ImportMode)
+                && ScheduleInterval > 0)
+            {
+                return true;
+            }
+            else
+            {
+                throw new ArgumentException($"Process exiting: Please make sure that the required configuration values are available.");
+            }
+        }
+    }
+}
